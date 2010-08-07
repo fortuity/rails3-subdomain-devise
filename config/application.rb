a79af8a91cddad27541a5ffa58c67fb7634a3528
Daniel Kehoe <kehoe@fortuity.com>
2010-08-07 12:06:09 -0700

    prevent logging of passwords

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,6 @@ module Rails3SubdomainDevise
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation]
   end
 end
