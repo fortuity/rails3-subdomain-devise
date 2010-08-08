f9773d17e2dbdccf5253eae1b7e25be01bf594dc
Daniel Kehoe <kehoe@fortuity.com>
2010-08-08 13:41:00 -0700

    Prevent Use of the Main Site With Subdomain URLs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   include UrlHelper
   protect_from_forgery
+  before_filter :limit_subdomain_access
+
+  protected
+
+    def limit_subdomain_access
+        if request.subdomain.present?
+          # this error handling could be more sophisticated!
+          # please make a suggestion :-)
+          redirect_to root_url(:subdomain => false)
+        end
+    end
+
 end
--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,4 +1,6 @@
 class SitesController < ApplicationController
+  skip_before_filter :limit_subdomain_access
+
   def show
     @site = Site.find_by_name!(request.subdomain)
   end
